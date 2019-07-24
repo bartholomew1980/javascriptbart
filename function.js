4054606330e6bb1fed71f94a260aef02ffa633ce
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-24 15:55:18 +0100

    functions and loops

---
+++ function.js
@@ -0,0 +1,78 @@
+/*
+for( i =0 ; i < 10 ; i++){
+    console.log(i);
+}
+//counts from 0-9. 
+
+
+
+
+
+
+
+for (i=9; i > -1; i--){
+
+
+console.log (i);
+}
+
+
+
+
+
+
+let favDrinks = [
+"coffee",
+"tea",
+"water"
+
+];
+
+console.log(favDrinks[0]);
+console.log(favDrinks[1]);
+console.log(favDrinks[2]);
+
+
+
+
+
+
+
+
+
+
+let favFilms = [
+"Rambo",
+"Rocky",
+"Titanic",
+
+];
+
+favFilms.push("Pianist"); 
+favFilms.push("Scream")
+
+for(filmIndex = 0; filmIndex < favFilms.length; filmIndex++){
+    console.log(favFilms[filmIndex])
+}
+
+
+
+
+*/
+
+let films = [
+    "Lets Be Cops",
+    "Taken",
+    "Ghostbusters"
+]
+        const filmCheck = () => {
+            if (films[2] == "Ghostbusters") {
+                console.log("Yay it's Ghostbusters");
+            } else {
+                console.log("Booo, we want Ghostbusters");
+            }
+        };
+        filmCheck();
+
+
+
---
+++ tomlesson.js
@@ -0,0 +1,91 @@
+/*
+
+let coffeIsGrinding = false;
+
+const pressGrindBeans = () => {
+if (coffeIsGrinding) {
+console.log( "Stopping the grind");
+coffeIsGrinding = false;
+
+} else {
+
+console.log("Grinding is about to begin");
+coffeIsGrinding = true;
+}
+
+}
+
+pressGrindBeans();
+
+
+
+
+
+
+let coffeOrder = (size, drinkType) =>{
+
+    console.log('Order received:  ${size} ${drinktype}');
+} 
+takeOrder("Tall", "Latte"); 
+
+
+
+
+
+
+
+
+const addUp = (num1, num2) => {
+    return num1 + num2; 
+}
+addU
+p(7,3); 
+console.log (addUp(2,5)); 
+
+
+
+
+const multipyByNineFifths = (celsius) => {
+     return celsius + (9/5);
+
+};
+
+const getFahrenheit = (celsius) ==> {
+     return multiplyByNineFifths(celsius) +32;
+    
+};
+
+console.log("the temperature is " +getFahrenheit(15) + "F")
+
+
+
+
+
+
+
+
+
+let orderCount = 0; 
+const takeOrder = (topping) => {
+
+    console.log('Pizza with ${topping}');
+} 
+orderCount ++;
+
+TakeOrder ("pineaple");
+
+
+
+
+
+const cashWithdrawal = (amount)
+
+
+
+
+
+function = subOrder (top1, top2, top3, top4, top5) =>{
+    console.log (we are proeparing uour sanwich ) 
+}
+
+*/
